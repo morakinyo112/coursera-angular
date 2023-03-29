d0d64e564ed13cfc7842407fe067a922033dd456
Mortech <akinsunday35@gmail.com>
2023-03-29 17:03:21 +0100

    Assignment folder updated

--- assignment1/app.js
+++ assignment1/app.js
@@ -1,22 +1,42 @@
-(function (){
-    'use strict';
-    
-    angular.module('MsgApp', [])
-    .controller('MsgController', MsgController);
-    
-    MsgController.$inject = ['$scope'];
-    function MsgController ($scope){
-        $scope.name = "Mortech";
-        $scope.stateOfBeing = "short";
+(function () {
+ 'use strict';
 
-        $scope.sayMessage = function (){
-            return "Mortech likes coding";
-        }
+    angular.module('LunchCheck', [])
+    .controller('LunchCheckController', LunchCheckController);
 
-        $scope.feedMortech = function (){
-            $scope.stateOfBeing = "full";
+    LunchCheckController.$inject = ['$scope']; 
+    function LunchCheckController ($scope){
+        $scope.lunchItems = "";
+        $scope.lunchMsg = "";
+        $scope.lunchColor = "";
+        $scope.lunchBorderColor = "";
+
+        $scope.lunchCheck = function (){
+            //$scope.lunchMsg = "We are coming up";
+            $scope.lunchMsg = setMessage($scope.lunchItems);
+        };
+
+        function setMessage(lunchItems) {
+            if(lunchItems.length==0){
+                $scope.lunchColor = "text-warning";
+                $scope.lunchBorderColor = "border-warning";
+                return "Please enter data first"
+            }
+           else {
+            var arr = lunchItems.split(",");
+            if(arr.length>3){
+                $scope.lunchColor = "text-danger";
+                $scope.lunchBorderColor = "border-danger";
+                return "Too much!"
+            }
+            else{
+                $scope.lunchColor = "text-success";
+                $scope.lunchBorderColor = "border-success";
+                return "Enjoy!"
+            }
+
+           }
         }
-    
     }
-    
-    })();
+
+})();
